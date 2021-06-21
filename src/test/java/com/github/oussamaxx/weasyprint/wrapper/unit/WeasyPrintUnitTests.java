1c1fa151a1ad9157cba468f837aaa15e951a3229
Oussama <norsessadaoui@gmail.com>
2021-06-21 22:37:58 +0100

    added params unit tests

---
+++ src/test/java/com/github/oussamaxx/weasyprint/wrapper/unit/WeasyPrintUnitTests.java
@@ -0,0 +1,28 @@
+package com.github.oussamaxx.weasyprint.wrapper.unit;
+
+import com.github.oussamaxx.weasyprint.wrapper.WeasyPrint;
+import com.github.oussamaxx.weasyprint.wrapper.params.Param;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.StringContains.containsString;
+
+public class WeasyPrintUnitTests {
+    private WeasyPrint wp;
+
+    @Before
+    public void setUp() {
+        wp = new WeasyPrint();
+    }
+
+    @Test
+    public void testParams() {
+
+        wp.addParams(new Param("--optimize-images"), new Param("--attachment", "test.txt"));
+        wp.addParam("-a", "oui.pdf");
+
+        Assert.assertThat("command params should contain the --optimize-images , --attachment and -a",
+                wp.getCommand(), containsString("--optimize-images --attachment test.txt -a oui.pdf"));
+    }
+}
